5a78a9eff410d453a686e496c5251570f73d8b32
Steven Ehrlich <ehrlichsteven@gmail.com>
2015-01-25 17:10:49 -0500

    hearts in semi-working non-scoring state

--- hearts.hs
+++ hearts.hs
@@ -1,9 +1,9 @@
--- import Data.Map.Strict as Z -- for Zones
-import Data.Set as Z
+import qualified Data.Map.Strict as B -- for Zones
+import qualified Data.Set as Z
 import Control.Monad (forM) -- liftM, unless
 import Data.Array.IO
-import Data.Maybe (isNothing)
-import Data.List (intercalate)
+import Data.Maybe (isNothing, fromJust)
+import Data.List (intercalate, maximumBy)
 import System.Random
 -- import Data.Vector 
 
@@ -36,7 +36,7 @@ instance Show Card where
                                     Hearts      -> ([1,31,47], "H")
                                     Diamonds    -> ([1,31,47], "D")
                         in colorize col $ ("-A23456789TJQKA"!!r) : pic
-        
+
 --type OZone = [Card]-- ordered Zones
 --type UZone = Z.Map ID Card -- unordered Zones
 type UZone = Z.Set Card
@@ -44,9 +44,10 @@ type UZone = Z.Set Card
 data Effect = Effect (World -> World) 
                 {-| Quit | Undo -}
                 | GetInput {-| Pass -}
+                | NewTrick
 
 data PassDir = PassLeft {-| PassRight | PassAcross | NoPass-}
-data Info = TrickInfo PlayerID (Maybe Suit) [Maybe Card] Scores
+data Info = TrickInfo PlayerID [(Card,PlayerID)] Scores | FirstTrick PlayerID
 data World = InRound Board Stack Info
             | StartGame 
             | StartRound PassDir Scores
@@ -54,9 +55,9 @@ data World = InRound Board Stack Info
 type Stack = [Effect]
 type Scores = [Int]
 
-type Board = [UZone]
+-- type Board = [UZone]
 -- type Board = [Zone]
--- type Board = Z.Map ID Zone
+type Board = B.Map PlayerID UZone
 -- type Board = Z.Map String OZone
 -- type Board = Z.Map ZoneID UZone
 
@@ -83,14 +84,11 @@ gameLoop ioworld = do
                         let h1 = Z.fromList $ take 13 $ drop 13 deck
                         let h2 = Z.fromList $ take 13 $ drop 26 deck
                         let h3 = Z.fromList $ take 13 $ drop 39 deck
-                        let board = [h0,h1,h2,h3]
+                        let board = B.insert 0 h0 $ B.insert 1 h1 $ B.insert 2 h2 $ B.insert 3 h3 $ B.empty
                         -- distribute deck to player hands
                         -- pass
                         let who_starts = 0 -- TODO see who has two of clubs
-                        let emptyInfo = TrickInfo who_starts Nothing 
-                                        [Nothing, Nothing, Nothing, Nothing]
-                                        scores
-                        gameLoop $ return $ InRound board [GetInput] emptyInfo
+                        gameLoop $ return $ InRound board [NewTrick] $ FirstTrick who_starts
                     where checkScores _ = False
                 InRound board (now:on_stack) info -> do
                     -- eventually this will be server code
@@ -99,12 +97,29 @@ gameLoop ioworld = do
                     let world' = InRound board on_stack info
 -- need to guarantee that stack is never empty
                     case now of 
+                        NewTrick ->
+                            -- compute winner
+                            -- add scores to current score
+                            -- add 4 get input to stack
+                            let w = computeWinner info
+                                s = computePoints info
+                                nextTrick = TrickInfo w [] s
+                                nextStep = if True --players have at least 1 card in hand 
+                                    then InRound board (GetInput:GetInput:GetInput:GetInput:NewTrick:on_stack) nextTrick
+                                    else GameOver [0,0,0,0]
+                                    --end round
+                            in
+                            gameLoop $ return  nextStep
                         GetInput -> do
+                            -- putStrLn "Get Input:"
                             -- get input from whomever cur_player is
                             -- right now its hot seat mode, so we ignore
                             player_input <- getMove world'
                             gameLoop $ return $ InRound board (player_input:on_stack) info
-                        Effect move -> gameLoop $ return $ move world 
+                        Effect move -> do 
+                            -- putStrLn "Executing Effect"
+                            -- _ <- getLine
+                            gameLoop $ return $ move world'
 
 getMove :: World -> IO Effect
 getMove w@(InRound board _stack info) = do
@@ -112,29 +127,45 @@ getMove w@(InRound board _stack info) = do
     if holds card && followsSuitIfAble card
     then return $ Effect (play card)
     else do 
+        -- putStrLn $ show (followsSuitIfAble card) ++ show (holds card)
         putStrLn "Illegal move:"
         getMove w
-    where TrickInfo cur_player lead _played _scores = info 
-          hand = board !! cur_player
+    where TrickInfo cur_player played _scores = info 
+          hand = fromJust $ B.lookup cur_player board 
           holds c = Z.member c hand
-          followsSuitIfAble card = 
-            case lead of
-                Nothing -> True
-                Just lead_suit -> 
-                    let matches_lead c = _suit c == lead_suit 
-                        has_no_lead = Z.foldr ((||).matches_lead) True hand
+          followsSuitIfAble card 
+            = if null played 
+                then True
+                else
+                    let lead_suit = _suit $ fst $ head played
+                        matches_lead c = _suit c == lead_suit
+                        has_lead = Z.foldr ((||).matches_lead) False hand
                     in
-                    matches_lead card || has_no_lead 
+                    matches_lead card || not has_lead 
 
+computeWinner :: Info -> PlayerID 
+computeWinner (FirstTrick holds2c) = holds2c
+computeWinner (TrickInfo _ played@((lead,_):_) _scores) =
+    let lead_suit = _suit lead
+        (_best_card, winner) = maximumBy (cmpWith lead_suit) played
+    in
+        winner
+    where cmpWith s (Card s1 r1,_) (Card s2 r2, _) | s2 == s1  = compare r1 r2 
+                                                   | s1 == s   = GT
+                                                   | otherwise = LT
+
+computePoints :: Info -> Scores
+computePoints _ = [0,0,0,0]
+
+    
 
 play :: Card -> World -> World
-play card (InRound board _stack (TrickInfo cur_player lead played scores)) = 
-    let new_board = board -- TODO remove card from board, add it to played
-        -- played !! cur_player = Just card
-        lead' = if isNothing lead then Just $ _suit card else lead
+play card (InRound board _stack (TrickInfo cur_player played scores)) = 
+    let new_board = B.adjust (Z.delete card) cur_player board 
+        new_played = played ++ [(card, cur_player)]
         next_player = (cur_player + 1) `mod` 4
     in
-        InRound new_board _stack (TrickInfo next_player lead' played scores)
+        InRound new_board _stack (TrickInfo next_player new_played scores)
 
 getInput :: IO Card
 getInput = do
@@ -176,29 +207,45 @@ readSuit s = case s of
 readRank :: Char -> Int
 readRank r 
         | r=='A' = 14
+        | r=='a' = 14
         | r=='K' = 13
+        | r=='k' = 13
         | r=='Q' = 12
+        | r=='q' = 12
         | r=='J' = 11
+        | r=='j' = 11
         | r=='T' = 10
+        | r=='t' = 10
         | r `elem` "23456789" = read [r] ::Int
 
 main :: IO ()
 main = gameLoop $ return StartGame
 
 render :: Board -> Info -> IO ()
-render [h0,h1,h2,h3] (TrickInfo cur_player _lead played [s0,s1,s2,s3]) = do 
+render board (TrickInfo cur_player played [s0,s1,s2,s3]) = do 
     -- if we should only be rendering the current players hand then do some checking
-    putStr $ "Player 0 Score:" ++ show s0 ++ " Hand: "
-    putStrLn $ unwords $ Prelude.map show $ Z.toList h0
-    putStr $ "Player 1 Score:" ++ show s1 ++ " Hand: "
-    putStrLn $ unwords $ Prelude.map show $ Z.toList h1
-    putStr $ "Player 2 Score:" ++ show s2 ++ " Hand: "
-    putStrLn $ unwords $ Prelude.map show $ Z.toList h2
-    putStr $ "Player 3 Score:" ++ show s3 ++ " Hand: "
-    putStrLn $ unwords $ Prelude.map show $ Z.toList h3
-    putStrLn $ "Waiting on" ++ show cur_player
-    putStrLn $ "Currently >" ++ show played
+    putStrLn "\ESC[H\ESC[2J"
+    putStrLn $ "Player 0 Score:" ++ show s0 
+    putStrLn $ "Player 1 Score:" ++ show s1
+    putStrLn $ "Player 2 Score:" ++ show s2
+    putStrLn $ "Player 3 Score:" ++ show s3
+    putStrLn $ "Waiting on " ++ show cur_player
+    putStrLn $ "Currently > " ++ show played
+    renderBoard board
     return ()
+
+render board (FirstTrick i) = do
+    putStrLn $ "Player " ++ show i ++ "leads the 2c"
+    renderBoard board   
+
+renderBoard :: Board -> IO ()
+renderBoard board = do
+    printHand 0
+    printHand 1
+    printHand 2
+    printHand 3
+    where printHand i = putStrLn $ (++) ( concat ["Player ", show i, " Hand: "] )
+                        $ unwords $ map show $ Z.toList $ fromJust (B.lookup i board)
 
 stdDeck :: [Card]
 ---- setting aces at 14
